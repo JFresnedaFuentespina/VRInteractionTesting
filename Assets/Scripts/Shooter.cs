5921d6b9596db84f0354f2ba2088261b5291a441
JFresnedaFuentespina <jfresnedafuentespina@gmail.com>
2025-12-04 14:03:36 +0100

    disparar con el trigger

--- Assets/Scripts/Shooter.cs
+++ Assets/Scripts/Shooter.cs
@@ -4,14 +4,31 @@ public class Shooter : MonoBehaviour
 {
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     public GameObject projectilePrefab;
+    public Transform spawnPoint;
+    private float bulletSpeed = 10f;
+    private OVRInput.Controller controller = OVRInput.Controller.RTouch; // Right-hand controller
+    private OVRInput.Button button = OVRInput.Button.PrimaryIndexTrigger; // Index trigger button
+    private bool buttonWasPressed = false;
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        bool buttonPressed = OVRInput.Get(button, controller);
+        if (buttonPressed && !buttonWasPressed)
+        {
+            Shoot();
+            // buttonWasPressed = buttonPressed;
+        }
+    }
+
+    void Shoot()
+    {
+        GameObject projectile = Instantiate(projectilePrefab, spawnPoint.position, spawnPoint.rotation);
+        Rigidbody rb = projectile.GetComponent<Rigidbody>();
+        rb.AddForce(spawnPoint.forward * bulletSpeed, ForceMode.Impulse);
     }
 }
