feccac66e35f51a95c73afd8d36ca44ec087031a
JFresnedaFuentespina <jfresnedafuentespina@gmail.com>
2026-01-15 12:48:59 +0100

    Refactor shooting logic to simplify button press handling

--- Assets/Scripts/Shooter.cs
+++ Assets/Scripts/Shooter.cs
@@ -8,20 +8,14 @@ public class Shooter : MonoBehaviour
     private float bulletSpeed = 10f;
     private OVRInput.Controller controller = OVRInput.Controller.RTouch; // Right-hand controller
     private OVRInput.Button button = OVRInput.Button.PrimaryIndexTrigger; // Index trigger button
-    private bool buttonWasPressed = false;
-    void Start()
-    {
-
-    }
 
     // Update is called once per frame
     void Update()
     {
         bool buttonPressed = OVRInput.GetDown(button, controller);
-        if (buttonPressed && !buttonWasPressed)
+        if (buttonPressed)
         {
             Shoot();
-            // buttonWasPressed = buttonPressed;
         }
     }
 
